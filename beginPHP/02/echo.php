bdc44334a4436c7fd4d954eb5fb9c41944e763b9
kovito <kgh961@naver.com>
2021-06-15 22:01:00 +0900

    2021-06-15(화)PHP웹 프로그래밍 기초 복습

---
+++ beginPHP/02/echo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>echo</title>
+    <script>
+
+    </script>
+  </head>
+  <body>
+    <h3>
+    <?php
+        echo "고양이와 토끼";
+    ?>
+    </h3>
+    <?php
+        
+    ?>
+  </body>
+</html>
